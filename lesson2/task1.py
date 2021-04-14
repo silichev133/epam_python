33f67e5a9965316c3321823b7d56e3c0326104b8
silich <silich@silichserver.moscow.epam.com>
2021-04-14 15:43:17 +0000

    Was changed task 1

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -1,7 +1,10 @@
 def create_dict(lst1, lst2):
+    if len(lst1) > len(lst2):
+        while len(lst1) > len(lst2):
+            lst2.append(None)
     return dict(zip(lst1, lst2))
 
-arr1 = [1, 2, 3]
+arr1 = [1, 2, 3, 4, 5]
 arr2 = ['a', 'b', 'c']
 
 d = create_dict(arr1, arr2)
